56668fe20bbede164d5d5ee1c23170c964c33323
Eric Andres <emandres@gmail.com>
2017-11-30 16:04:10 -0700

    Rough out the fulfillment response

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 let app = express()
 
+app.use(bodyParser.json())
 
 app.post('/link', (req, res) => {
     res.send(JSON.stringify({
@@ -13,10 +15,18 @@ app.post('/link', (req, res) => {
 })
 
 app.post('/fulfillment', (req, res) => {
-    res.send(JSON.stringify({
-        headers: req.headers,
-        body: req.body
-    }))
+    let request = req.body
+    let response
+    switch (request.operation) {
+        case "SubscriptionActivate":
+            response = { response: "OK" }
+            break
+        case "SubscriptionDeactivate":
+            response = { response: "OK" }
+            break
+    }
+    console.log(response)
+    res.send(JSON.stringify(response))
 })
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
