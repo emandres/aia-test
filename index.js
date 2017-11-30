8a3efbf242688f4fdb8193136200168ac30eed3d
Eric Andres <emandres@gmail.com>
2017-11-30 15:34:31 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+let app = express()
+
+app.get('/', (req, res) => {
+    res.send(JSON.stringify(req.headers))
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
